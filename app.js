369c47b812890762349f9b50431324ece26b35f8
markal307 <54350886+markal307@users.noreply.github.com>
2019-12-06 13:50:21 +0100

    fixes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const MartinRemote = "192.168.250.60";
 const AlbinRemote = "192.168.250.52";
 const Martin = "192.168.2.199";
 const local = "localhost";
-const host = MartinRemote;
+const host = local;
 const app = express();
 
 const server = app.listen(port, host, function() {
@@ -37,9 +37,6 @@ app.get("/users", function(req, res) {
 });
 
 app.set("view engine", "ejs");
-app.get("/ejs", (req, res) => {
-  res.render("index", { data: "hello world!" });
-});
 
 app.use(
   session({
@@ -49,36 +46,8 @@ app.use(
   })
 );
 
-app.get("/session", (req, res, next) => {
-  req.session.viewCount += 1;
-  res.render("test.ejs", { viewCount: req.session.viewCount });
-});
-
-app.get("/login", (req, res, next) => {
-  res.render("login.ejs", { host: host });
-});
-
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-
-app.post("/dashboard", async function(req, res) {
-  if (await db.login(req.body.username, req.body.password)) {
-    req.session.username = req.body.username;
-    req.session.password = req.body.password;
-    res.render("dashboard.ejs", {
-      username: req.session.username,
-      password: req.session.password,
-      helpers: ejsHelpers
-    });
-    io.on("connection", function(socket) {
-      chat.addClient(socket.id, req.session.username,io);
-    });
-  } else {
-    req.session.username = req.body.username;
-    req.session.password = req.body.password;
-    res.render("error.ejs");
-  }
-});
 
 app.post("/req", async function(req, res) {
   if (req.body.password) {
