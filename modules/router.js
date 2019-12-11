f79f1ec0021c8d56b38363d1b64650d506a59af2
markal307 <54350886+markal307@users.noreply.github.com>
2019-12-11 12:36:48 +0100

    Cleaned the code in app

---
+++ modules/router.js
@@ -0,0 +1,79 @@
+const db = require("./../db/db");
+const chat = require("./chatManager");
+const ejsHelpers = require("./../views/helpers/createModal");
+
+const home = async (req,res,io) => {
+    if (req.session.username) {
+        if (await db.login(req.session.username, req.session.password) && req.session.password) {
+            res.render("dashboard.ejs", {
+                username: req.session.username,
+                helpers: ejsHelpers
+            });
+            io.on("connection", function(socket) {
+                chat.addClient(socket.id, req.session.username,io);
+            });
+        } else if (!req.session.password) {
+            res.render("dashboard.ejs", {
+                username: req.session.username,
+                helpers: ejsHelpers
+            });
+            io.on("connection", function(socket) {
+                chat.addClient(socket.id, req.session.username,io);
+            });
+        } else {
+            res.render("error.ejs");
+        }
+    } else {
+        res.render("dashboard.ejs", { username: null, helpers: ejsHelpers });
+    }
+};
+
+const createAccount = async (req,res)=>{
+    if (await db.addUser(req.body.username, req.body.password)) {
+        req.session.username = req.body.username;
+        req.session.password = req.body.password;
+        await res.json(req.body.username);
+        chat.addClient(req.body.socket, req.body.username,io);
+    } else {
+        req.session.username = req.body.username;
+        req.session.password = req.body.password;
+        await res.json(false);
+    }
+};
+
+const loginAccount = async (req,res)=>{
+    if (req.body.password) {
+        if (await db.login(req.body.username, req.body.password)) {
+            req.session.username = req.body.username;
+            req.session.password = req.body.password;
+            await res.json(req.body.username);
+            chat.addClient(req.body.socket, req.body.username,io);
+        } else {
+            req.session.username = req.body.username;
+            req.session.password = req.body.password;
+            await res.json(false);
+        }
+    } else {
+        console.log(req.body.username);
+        if (req.body.username) {
+            req.session.username = req.body.username + "_guest";
+            await res.json(req.session.username);
+            chat.addClient(req.body.socket, req.session.username,io);
+        } else {
+            res.json(false);
+        }
+    }
+};
+
+const users = (req,res)=>{
+        db.getAllUsers().then(r => {
+            res.render("index", { data: r });
+        });
+};
+
+module.exports = {
+    home,
+    createAccount,
+    loginAccount,
+    users
+};
