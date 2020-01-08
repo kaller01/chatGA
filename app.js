260fc620bc8a1b5b34d6ec58283f80acbb65172d
albarv340 <albarv340@users.noreply.github.com>
2020-01-08 13:02:50 +0100

    Hid the controllers when loading
    Hid the controllers to the video player when the page is loading so that nothing breaks from the suer trying to use the controllers

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const MartinRemote = "192.168.250.60";
 const AlbinRemote = "192.168.250.52";
 const Martin = "192.168.2.199";
 const local = "localhost";
-const host = MartinRemote;
+const host = AlbinRemote;
 
 
 const server = app.listen(port, host, function() {
