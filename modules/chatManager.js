210b6e36ddcfb4adcc5e3d552e078becd5f2c809
markal307 <54350886+markal307@users.noreply.github.com>
2020-02-12 13:18:07 +0100

    New structure
    Reamaking the way we send sockets, more structured and more compact.
    
    JAG BLIR GALEn
    den skickar två meddelanden????

--- modules/chatManager.js
+++ modules/chatManager.js
@@ -6,153 +6,167 @@ const db = require("../db/db");
 const linkPreview = require("./link");
 
 //Object Client
-let Client = function(socketid, name) {
-  let id = socketid;
-  let username = name;
-  this.getId = function() {
-    return id;
-  };
-  this.getUsername = function() {
-    return username;
-  };
+let Client = function (socketid, name) {
+    let id = socketid;
+    let username = name;
+    this.getId = function () {
+        return id;
+    };
+    this.getUsername = function () {
+        return username;
+    };
 };
 
 //Array with clients
 let clients = [];
 
-let msg = function(fromId, rawMessage, io) {
-  //Uses stripHtml to remove all html tags and then uses anchrome to put <a> around links
-  const message = anchorme(stripHtml(rawMessage));
+let msg = function (fromId, data, io) {
+    //Uses stripHtml to remove all html tags and then uses anchrome to put <a> around links
+    const rawMessage = data.message;
+    const message = anchorme(stripHtml(rawMessage));
 
-  if (typeof clients[fromId] == "undefined") {
-    io.to(fromId).emit("chatMessage", {
-      message: "authorization needed",
-      username: "system"
-    });
-  } else {
-    //checks for /msg
-    if (rawMessage.startsWith("/")) {
-      switch (rawMessage.split(" ")[0]) {
-        case "/msg":
-          command.private(fromId, message, io, clients);
-          break;
-        case "/rickroll":
-          command.rickroll(fromId, message, io, clients);
-          break;
-        case "/clients":
-          command.users(fromId, message, io, clients);
-          break;
-        case "/modal":
-          command.modal(fromId, message, io, clients);
-          break;
-        case "/watch":
-          command.watch(fromId, rawMessage, io, clients);
-          break;
-      }
-    } else {
-      if (dev.logMessages) {
-        let fromUser = clients[fromId].getUsername();
-        let date = Date.now();
-        db.addMessage(fromUser, "all", message, date);
-      }
-
-      //checks for /msg
-      if (rawMessage.startsWith("/")) {
-        switch (rawMessage.split(" ")[0]) {
-          case "/msg":
-            command.private(fromId, message, io, clients);
-            break;
-          case "/rickroll":
-            command.rickroll(fromId, message, io, clients);
-            break;
-          case "/clients":
-            command.users(fromId, message, io, clients);
-            break;
-          case "/modal":
-            command.modal(fromId, message, io, clients);
-            break;
-          case "/watch":
-            command.watch(fromId, rawMessage, io, clients);
-            break;
-        }
-      } else {
-        const message = anchorme(stripHtml(rawMessage));
-        linkPreview.messageToLink(message, io).catch(e => {});
-        io.emit("chatMessage", {
-          message: message,
-          username: clients[fromId].getUsername()
+    if (typeof clients[fromId] == "undefined") {
+        io.to(fromId).emit("chatMessage", {
+            message: "authorization needed",
+            username: "system"
         });
-      }
+    } else {
+        //checks for /msg
+        if (rawMessage.startsWith("a  /")) {
+            switch (rawMessage.split(" ")[0]) {
+                case "/msg":
+                    console.log("called");
+                    command.private(fromId, message, io, clients);
+                    break;
+                case "/rickroll":
+                    command.rickroll(fromId, message, io, clients);
+                    break;
+                case "/clients":
+                    command.users(fromId, message, io, clients);
+                    break;
+                case "/modal":
+                    command.modal(fromId, message, io, clients);
+                    break;
+                case "/watch":
+                    command.watch(fromId, rawMessage, io, clients);
+                    break;
+            }
+        } else {
+            const message = anchorme(stripHtml(rawMessage));
+            const messagedata = {
+                message,
+                receiver: data.receiver,
+                sender: clients[fromId].getUsername(),
+            };
+            if (data.receiver !== "all") {
+                //lops through each client and checks username
+                emitMessage(messagedata, io, fromId);
+                Object.keys(clients).forEach(function (id) {
+                    // console.log(clients[id].getUsername());
+                    if (clients[id].getUsername() === data.receiver) {
+                        emitMessage(messagedata, io, id);
+                    }
+                });
+            } else emitMessage(messagedata, io);
+
+            if (dev.logMessages) {
+                let fromUser = clients[fromId].getUsername();
+                let date = Date.now();
+                db.addMessage(fromUser, data.receiver, message, date);
+            }
+        }
     }
-  }
 };
 
-const addClient = function(socketid, username, io) {
-  clients[socketid] = new Client(socketid, username);
-  // console.log("New user: " + clients[socketid].getUsername() + " " + clients[socketid].getId());
-  updateClientList(clients, io);
+const addClient = function (socketid, username, io) {
+    clients[socketid] = new Client(socketid, username);
+    // console.log("New user: " + clients[socketid].getUsername() + " " + clients[socketid].getId());
+    updateClientList(clients, io);
 };
 
 const updateClientList = async (clients, io) => {
-  let clientList = [];
-  Object.keys(clients).forEach(function(id) {
-    clientList.push(clients[id].getUsername());
-  });
-  io.emit("chatUpdate", {
-    clients: clientList
-  });
-  // console.log(clients);
+    let clientList = [];
+    Object.keys(clients).forEach(function (id) {
+        clientList.push(clients[id].getUsername());
+    });
+    io.emit("chatUpdate", {
+        clients: clientList
+    });
+    // console.log(clients);
 };
 
 const getLastMessages = async (id, io, sender = null, to = "all") => {
-  if (sender) {
+    if (sender) {
+        const messages = await db.getLastMessages("all");
+    }
     const messages = await db.getLastMessages("all");
-  }
-  const messages = await db.getLastMessages("all");
-  io.to(id).emit("lastMessages", {
-    messages
-  });
+    io.to(id).emit("lastMessages", {
+        messages
+    });
 };
 
-const disconnectClient = function(socketid, io) {
-  if (clients[socketid]) {
-    // console.log("Removed user: " + socketid);
-    delete clients[socketid];
-    updateClientList(clients, io);
-  }
+const disconnectClient = function (socketid, io) {
+    if (clients[socketid]) {
+        // console.log("Removed user: " + socketid);
+        delete clients[socketid];
+        updateClientList(clients, io);
+    }
 };
 
 async function logintoDB(socketid, io, username, password) {
-  let login = await db.login(username, password);
-  if (login) {
-    // console.log(username + " logged in: " + login);
-    addClient(socketid, username);
-    io.to(socketid).emit("loginAccount", true);
-  } else {
-    io.to(socketid).emit("loginAccount", false);
-  }
+    let login = await db.login(username, password);
+    if (login) {
+        // console.log(username + " logged in: " + login);
+        addClient(socketid, username);
+        io.to(socketid).emit("loginAccount", true);
+    } else {
+        io.to(socketid).emit("loginAccount", false);
+    }
 }
 
 async function createUserDB(socketid, io, username, password) {
-  // username = username.replace(/[ :?]/g, '');
-  // console.log(username);
-  if (typeof username !== "undefined" || username.length > 3) {
-    let added = await db.addUser(username, password);
-    // console.log(added);
-    if (added) {
-      io.to(socketid).emit("createAccount", true);
-      addClient(socketid, username);
-    } else {
-      io.to(socketid).emit("createAccount", false);
+    // username = username.replace(/[ :?]/g, '');
+    // console.log(username);
+    if (typeof username !== "undefined" || username.length > 3) {
+        let added = await db.addUser(username, password);
+        // console.log(added);
+        if (added) {
+            io.to(socketid).emit("createAccount", true);
+            addClient(socketid, username);
+        } else {
+            io.to(socketid).emit("createAccount", false);
+        }
     }
-  }
 }
 
+//takes an io object
+const emitMessage = (data, io, id = 0) => {
+    const socketchannel = "chatMessage";
+    const messagedata = {
+        message: data.message,
+        username: data.sender,
+        receiver: {
+            username: data.receiver
+        },
+    };
+    if (id) {
+        console.log(id);
+        io.to(id).emit(socketchannel, messagedata);
+        linkPreview.messageToLink(data.message, io.to(id)).catch(e => {
+        });
+    } else {
+        io.emit(socketchannel, messagedata);
+        linkPreview.messageToLink(data.message, io).catch(e => {
+        });
+    }
+};
+
+
 module.exports = {
-  msg: msg,
-  addClient: addClient,
-  disconnect: disconnectClient,
-  login: logintoDB,
-  createUser: createUserDB,
-  getLastMessages
+    msg: msg,
+    addClient: addClient,
+    disconnect: disconnectClient,
+    login: logintoDB,
+    createUser: createUserDB,
+    getLastMessages
 };
--- public/events.js
+++ public/events.js
@@ -1,121 +1,119 @@
 let sendTo = "all";
+let clientUsername = $("#displayName").text();
 
 window.addEventListener("DOMSubtreeModified", () => {
-  $('a[data-toggle="tab"]').on("shown.bs.tab", function(e) {
-    let user = e.target.attributes.id.value.replace("-tab", "");
-    if (sendTo !== user) {
-      console.log("Changeconvo", user);
-      clearNotifications(user);
-      sendTo = user;
-    }
-  });
-  $(".previewImg").click(function(event) {
-    let filename = event.target.src;
-    $(".bigImg").attr("style", "display:flex");
-    $("#bigImg").attr("src", filename);
-  });
+    $('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+        let user = e.target.attributes.id.value.replace("-tab", "");
+        if (sendTo !== user) {
+            console.log("Changeconvo", user);
+            clearNotifications(user);
+            sendTo = user;
+        }
+    });
+    $(".previewImg").click(function (event) {
+        let filename = event.target.src;
+        $(".bigImg").attr("style", "display:flex");
+        $("#bigImg").attr("src", filename);
+    });
 });
 
-chatForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-  // const user = $("#chatInput2").val();
-  const user = sendTo;
-  let message = chatInput.value;
-  if (user === "all") {
+chatForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // const user = $("#chatInput2").val();
+    const user = sendTo;
+    let message = chatInput.value;
+    console.log(message);
+    if (user !== "all") {
+        newConversation(user);
+    }
     socket.emit("chatMessage", {
-      message
+        message,
+        receiver: user
     });
-  } else {
-    message = "/msg " + user + " " + message;
-    newConversation(user);
-    socket.emit("chatMessage", {
-      message,
-      channel: user
-    });
-  }
 
-  if (messageHistory[messageHistory.length - 1] !== chatInput.value) {
-    messageHistory.push(chatInput.value);
-  }
-  messageNumber = messageHistory.length;
-  chatInput.value = "";
+    if (messageHistory[messageHistory.length - 1] !== chatInput.value) {
+        messageHistory.push(chatInput.value);
+    }
+    messageNumber = messageHistory.length;
+    chatInput.value = "";
 });
 
 $("#chatForm").keydown(e => {
-  if (e.key === "ArrowUp") {
-    if (messageNumber > 0) messageNumber--;
-    e.preventDefault();
-    chatInput.value = messageHistory[messageNumber];
-    setCaretPosition(chatInput, chatInput.value.length);
-  } else if (e.key === "ArrowDown") {
-    if (messageNumber < messageHistory.length - 1 && messageNumber >= 0) {
-      messageNumber++;
-      chatInput.value = messageHistory[messageNumber];
-    } else if (messageNumber === messageHistory.length - 1) {
-      messageNumber++;
-      chatInput.value = "";
+    if (e.key === "ArrowUp") {
+        if (messageNumber > 0) messageNumber--;
+        e.preventDefault();
+        chatInput.value = messageHistory[messageNumber];
+        setCaretPosition(chatInput, chatInput.value.length);
+    } else if (e.key === "ArrowDown") {
+        if (messageNumber < messageHistory.length - 1 && messageNumber >= 0) {
+            messageNumber++;
+            chatInput.value = messageHistory[messageNumber];
+        } else if (messageNumber === messageHistory.length - 1) {
+            messageNumber++;
+            chatInput.value = "";
+        }
     }
-  }
 });
 
-$("#exampleModal").on("click", function(e) {
-  $("#modal-body").html("");
-  $("#exampleModal").modal("hide");
+$("#exampleModal").on("click", function (e) {
+    $("#modal-body").html("");
+    $("#exampleModal").modal("hide");
 });
 
 function setCaretPosition(ctrl, pos) {
-  // Modern browsers
-  if (ctrl.setSelectionRange) {
-    ctrl.focus();
-    ctrl.setSelectionRange(pos, pos);
+    // Modern browsers
+    if (ctrl.setSelectionRange) {
+        ctrl.focus();
+        ctrl.setSelectionRange(pos, pos);
 
-    // IE8 and below
-  } else if (ctrl.createTextRange) {
-    var range = ctrl.createTextRange();
-    range.collapse(true);
-    range.moveEnd("character", pos);
-    range.moveStart("character", pos);
-    range.select();
-  }
+        // IE8 and below
+    } else if (ctrl.createTextRange) {
+        var range = ctrl.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select();
+    }
 }
-$(".bigImg").click(function(event) {
-  $(".bigImg").hide();
+
+$(".bigImg").click(function (event) {
+    $(".bigImg").hide();
 });
 
 $(document).keydown(e => {
-  if (e.key === "Escape" && $(".bigImg").is(":visible")) {
-    $(".bigImg").hide();
-  }
+    if (e.key === "Escape" && $(".bigImg").is(":visible")) {
+        $(".bigImg").hide();
+    }
 });
 
 
-$("#modalLogin").on("show.bs.modal", function(e) {
-  setTimeout(() => {
-    document.getElementById("username").focus();
-  }, 50);
+$("#modalLogin").on("show.bs.modal", function (e) {
+    setTimeout(() => {
+        document.getElementById("username").focus();
+    }, 50);
 });
 
-$("#modalCreate").on("show.bs.modal", function(e) {
-  setTimeout(() => {
-    document.getElementById("usernameCreate").focus();
-  }, 50);
+$("#modalCreate").on("show.bs.modal", function (e) {
+    setTimeout(() => {
+        document.getElementById("usernameCreate").focus();
+    }, 50);
 });
 
-$("#modalGuest").on("show.bs.modal", function(e) {
-  setTimeout(() => {
-    document.getElementById("usernameGuest").focus();
-  }, 50);
+$("#modalGuest").on("show.bs.modal", function (e) {
+    setTimeout(() => {
+        document.getElementById("usernameGuest").focus();
+    }, 50);
 });
 
-const clearNotifications = username =>{
-  $("#"+username+"-notification").hide();
+const clearNotifications = username => {
+    $("#" + username + "-notification").hide();
 };
 
-const addNotifcations = username =>{
-    if(sendTo !== username){
-        let notification = $("#"+username+"-notification");
+const addNotifcations = username => {
+    if (sendTo !== username) {
+        let notification = $("#" + username + "-notification");
         notification.show();
-        notification.text(parseInt(notification.text())+1);
+        notification.text(parseInt(notification.text()) + 1);
     }
 };
 
