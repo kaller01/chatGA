8ebc903cfb14e2d72a8267a153d707a40e1a993d
markal307 <54350886+markal307@users.noreply.github.com>
2020-02-26 14:30:27 +0100

    uhm this is actually game
    canvas learning
    removed unused files
    simple start for ping pong, 2 users can move. Frontend only sends bool

--- modules/socketManager.js
+++ modules/socketManager.js
@@ -18,6 +18,31 @@ const manager = function(socket, io) {
     chat.getLastMessages(socket.id, io);
   });
 
+  let p1 = {
+    y: 100,
+  };
+
+  socket.on("pingpong",(data)=>{
+
+    data.up ? p1.y-=3 : false;
+    data.down ? p1.y+=3 : false;
+
+    const gameData = {
+      p1
+    };
+
+    socket.emit("pingpong",{
+      p1: p1,
+      p2: false
+    });
+    socket.broadcast.emit("pingpong",{
+      p2: p1,
+      p1: false,
+    })
+
+
+  });
+
   socket.on("player", function(data) {
     player.playing = data.playing;
     player.position = data.position;
--- views/helpers/createModal.js
+++ views/helpers/createModal.js
@@ -1,8 +1,8 @@
-const createModal = (id,header,body,button)=>{
-  let html = `
+const createModal = (id, header, body, button = "") => {
+    let html = `
   <!-- Modal -->
     <div class="modal fade" id="modal${id}" tabindex="-1" role="dialog" aria-labelledby="modal${header}" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+        <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalLabel${id}">${header}</h5>
@@ -15,14 +15,17 @@ const createModal = (id,header,body,button)=>{
                     <p class="error" style="display:none;" id=error${id}>Error</p>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" id="modalButton${id}">${button}</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>`;
+    if (button) {
+        html += `<button type="button" class="btn btn-primary" id="modalButton${id}">${button}</button>`;
+    }
+    html += `                   
                 </div>
             </div>
         </div>
     </div>
   `;
-return html;
+    return html;
 
 };
 
